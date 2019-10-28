a15b74f8eba4ef62911b5d81943034d3e7a43416
Jacob Berger <jberger.1919@gmail.com>
2019-10-28 09:47:28 -0700

    Filled out Question

--- mazeUtils/Question.java
+++ mazeUtils/Question.java
@@ -1,5 +1,45 @@
 package mazeUtils;
 
-public class Question {
+	public class Question {
+	
+	private String type;
+	private String question;
+	private String answer;
 
+	protected Question(String type, String question, String answer) {
+		this.type = type;
+		this.question = question;
+		this.answer = answer;
+	}
+	
+	protected Question(String type) {
+		this.type = type;
+		this.question = "DEFAULT QUESTION";
+		this.answer = "DEFAULT ANSWER";
+	}
+	
+	protected String getType() {
+		return this.type;
+	}
+	
+	private void setType(String type) {
+		this.type = type;
+	}
+	
+	protected String getQuestion() {
+		return this.question;
+	}
+	
+	private void setQuestion(String question) {
+		this.question = question;
+	}
+	
+	protected String getAnswer() {
+		return this.answer;
+	}
+	
+	private void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
 }
