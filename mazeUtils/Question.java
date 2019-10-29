0f53131a764f4788fe3f78b9dad87d103a640ab7
Jacob Berger <jberger.1919@gmail.com>
2019-10-29 12:30:48 -0700

    Mostly finished Question class
    Uncomment main() to see the output. Does not randomize response order for multiple choice questions yet.

--- mazeUtils/Question.java
+++ mazeUtils/Question.java
@@ -1,55 +1,131 @@
 package mazeUtils;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-	public class Question {
-	
+public class Question {
+
 	private QuestionType type;
 	private String question;
 	private String answer;
-	private String database;
+	private String response1;
+	private String response2;
+	private String response3;
+	private Connection database;
 
-	protected Question(QuestionType type, String question, String answer) throws NullPointerException, IllegalArgumentException {
+	protected Question(QuestionType type) throws NullPointerException, IllegalArgumentException {
 		if (type == null) {
 			throw new NullPointerException("The type is null");
-		} else if (question == null) {
-			throw new NullPointerException("The question is null");
-		} else if (question.length() == 0) {
-			throw new IllegalArgumentException("The question is empty");
-		} else if (answer == null) {
-			throw new NullPointerException("The answer is null");
-		} else if (answer.length() == 0) {
-			throw new IllegalArgumentException("The answer is empty");
 		}
 		this.type = type;
-		this.question = question;
-		this.answer = answer;
+
+		ArrayList<String> results = this.retrieveQuestion(type);
+		this.question = results.get(0);
+		this.answer = results.get(1);
+		if (type == QuestionType.MULTIPLE_CHOICE) {
+			this.response1 = results.get(2);
+			this.response2 = results.get(3);
+			this.response3 = results.get(4);
+		}
 	}
-	
-	protected Question(QuestionType type) {
-		new Question(type, "DEFAULT QUESTION", "DEFAULT ANSWER");
-	}
-	
+
 	protected QuestionType getType() {
 		return this.type;
 	}
-	
+
 	private void setType(QuestionType type) {
 		this.type = type;
 	}
-	
+
 	protected String getQuestion() {
 		return this.question;
 	}
-	
+
 	private void setQuestion(String question) {
 		this.question = question;
 	}
-	
+
 	protected String getAnswer() {
 		return this.answer;
 	}
-	
+
 	private void setAnswer(String answer) {
 		this.answer = answer;
 	}
-	
+
+	private ArrayList<String> retrieveQuestion(QuestionType type) {
+		ArrayList<String> results = new ArrayList<String>();
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			database = DriverManager.getConnection("jdbc:sqlite:database/trivia.db");
+			Statement statement = database.createStatement();
+
+			//Get total number of questions in table
+			ResultSet resultSet = statement.executeQuery("select count(*) from " + type.toString());
+			int totalQuestions = resultSet.getInt("count(*)");
+			System.out.println("Total questions " + totalQuestions);
+
+			//Get random question
+			double randomDouble = Math.random();
+			randomDouble = randomDouble * totalQuestions + 1;
+			int question_number = (int) randomDouble;
+			System.out.println("Question number " + question_number);
+			System.out.println();
+			resultSet = statement.executeQuery("select * from " + type.toString() + " where question_number = " + question_number);
+			results.add(resultSet.getString("question"));
+			results.add(resultSet.getString("answer"));
+
+			if (this.type == QuestionType.MULTIPLE_CHOICE) {
+				results.add(resultSet.getString("response1"));
+				results.add(resultSet.getString("response2"));
+				results.add(resultSet.getString("response3"));
+			}
+
+			statement.close();
+			database.close();
+		} catch (Exception e) {
+			System.out.println("There was an error with the database connection");
+		}
+
+		return results;
+	}
+
+	public String toString() {
+		String result = "";
+		result += "Type: " + this.getType() + "\n";
+		result += "Question: " + this.getQuestion() + "\n";
+
+		switch (this.type.toString()) {
+		case "TRUE_FALSE":
+			result += "True or false?\n";
+			break;
+
+		case "MULTIPLE_CHOICE":
+			//Prints in same order every run
+			result += this.response1 + "\n";
+			result += this.response2 + "\n";
+			result += this.response3 + "\n";
+			result += this.answer + "\n";
+			result += "Type your response\n";
+			break;
+
+		case "SHORT_ANSWER":
+			result += "Type your response\n";
+		}
+
+		return result;
+	}
+
+//	public static void main(String[] args) {
+//		Question question = new Question(QuestionType.TRUE_FALSE);
+//		System.out.println(question.toString());
+//		System.out.println();
+//		question = new Question(QuestionType.MULTIPLE_CHOICE);
+//		System.out.println(question.toString());
+//		System.out.println();
+//		question = new Question(QuestionType.SHORT_ANSWER);
+//		System.out.println(question.toString());
+//	}
+
 }
