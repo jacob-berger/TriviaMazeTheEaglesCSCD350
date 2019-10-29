3fe9d2420970b9587d1cb94930aa22d355038c72
Jacob Berger <jberger.1919@gmail.com>
2019-10-29 09:42:57 -0700

    Added database to project file.
    Inserting filler questions for each table (TRUE_FALSE, MULTIPLE_CHOICE, SHORT_ANSWER). Requires SQlite install.

--- mazeUtils/Question.java
+++ mazeUtils/Question.java
@@ -2,27 +2,37 @@ package mazeUtils;
 
 	public class Question {
 	
-	private String type;
+	private QuestionType type;
 	private String question;
 	private String answer;
+	private String database;
 
-	protected Question(String type, String question, String answer) {
+	protected Question(QuestionType type, String question, String answer) throws NullPointerException, IllegalArgumentException {
+		if (type == null) {
+			throw new NullPointerException("The type is null");
+		} else if (question == null) {
+			throw new NullPointerException("The question is null");
+		} else if (question.length() == 0) {
+			throw new IllegalArgumentException("The question is empty");
+		} else if (answer == null) {
+			throw new NullPointerException("The answer is null");
+		} else if (answer.length() == 0) {
+			throw new IllegalArgumentException("The answer is empty");
+		}
 		this.type = type;
 		this.question = question;
 		this.answer = answer;
 	}
 	
-	protected Question(String type) {
-		this.type = type;
-		this.question = "DEFAULT QUESTION";
-		this.answer = "DEFAULT ANSWER";
+	protected Question(QuestionType type) {
+		new Question(type, "DEFAULT QUESTION", "DEFAULT ANSWER");
 	}
 	
-	protected String getType() {
+	protected QuestionType getType() {
 		return this.type;
 	}
 	
-	private void setType(String type) {
+	private void setType(QuestionType type) {
 		this.type = type;
 	}
 	
